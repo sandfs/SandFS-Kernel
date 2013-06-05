e4f09f1e8e14616754d868b69068e3dbbea53bd0
Erez Zadok <ezk@cs.sunysb.edu>
2013-06-04 23:19:27 -0400

    Wrapfs: use clear_inode in evict_inode
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- fs/wrapfs/super.c
+++ fs/wrapfs/super.c
@@ -84,7 +84,7 @@ static void wrapfs_evict_inode(struct inode *inode)
 	struct inode *lower_inode;
 
 	truncate_inode_pages(&inode->i_data, 0);
-	end_writeback(inode);
+	clear_inode(inode);
 	/*
 	 * Decrement a reference to a lower_inode, which was incremented
 	 * by our read_inode when it was created initially.
