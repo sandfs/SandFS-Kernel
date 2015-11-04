a98128ac929089cd991d727058a536438f745186
Erez Zadok <ezk@cs.sunysb.edu>
2015-11-03 21:19:11 -0500

    Wrapfs: update copyright year to 2015

--- fs/wrapfs/lookup.c
+++ fs/wrapfs/lookup.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 1998-2014 Erez Zadok
+ * Copyright (c) 1998-2015 Erez Zadok
  * Copyright (c) 2009	   Shrikar Archak
- * Copyright (c) 2003-2014 Stony Brook University
- * Copyright (c) 2003-2014 The Research Foundation of SUNY
+ * Copyright (c) 2003-2015 Stony Brook University
+ * Copyright (c) 2003-2015 The Research Foundation of SUNY
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
